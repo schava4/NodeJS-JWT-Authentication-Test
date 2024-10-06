bcc58615a5f00aa99176762e1b2262cb118ed718
schava4 <schava2@charlotte.edu>
2024-10-06 17:33:35 -0400

    Completed instructions

--- server.js
+++ server.js
@@ -1,97 +1,101 @@
-const express = require("express");
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser');
 const app = express();
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
+})
+const jwt = require('jsonwebtoken')
+const { expressjwt: exjwt } = require('express-jwt');
 
-const jwt = require("jsonwebtoken");
-const { expressjwt: expressJwt } = require("express-jwt");
-
-const bodyParser = require("body-parser");
-const path = require("path");
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3010");
-  res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
-  next();
-});
+const PORT = 3010;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 
-const port = 3010;
-const secretKey = "My super secret Key";
-const jwtMW = expressJwt({
-  secret: secretKey,
-  algorithms: ["HS256"],
+
+const secretKey = "My Secret Key";
+const jwtMW = exjwt({
+    secret: secretKey,
+    algorithms: ['HS256'],
 });
 
 let users = [
-  {
-    id: 1,
-    username: "sriya",
-    password: "123",
-  },
-  {
-    id: 2,
-    username: "dhruv",
-    password: "456",
-  },
-];
-
-app.post("/api/login", (req, res) => {
-  const { username, password } = req.body;
-
-  for (let user of users) {
-    if (username === user.username && password === user.password) {
-      let token = jwt.sign(
-        { id: user.id, username: user.username },
-        secretKey,
-        { expiresIn: "7d" }
-      );
-      res.json({
-        success: true,
-        err: null,
-        token,
-      });
-      break;
-    } else {
-      res.status(401).json({
-        success: false,
-        token: null,
-        err: "Username or password is incorrect",
-      });
+    {
+        id:1,
+        username:'sriya',
+        password:'123'
+    },
+    {
+        id:2,
+        username:'dhruva',
+        password:'456'
     }
-  }
+]
+
+app.get("/api/dashboard", jwtMW, (req,res)=>{
+    console.log(req);
+    res.json({
+        success:true,
+        myContent:"Secret Content that only logged in people can view"
+    })
+})
+
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err.name==='UnauthorizedError');
+    console.log(err);
+    if(err.name === 'UnauthorizedError'){
+        res.status(401).json({
+            success:false,
+            officialErr: err,
+            err:"2. Username or password is incorrect"
+        })
+    }
+    else{
+        next(err);
+    }
 });
 
-app.get("/api/dashboard", jwtMW, (req, res) => {
-  res.json({
-    success: true,
-    myContent: "Secret content that only logged in people can see.",
-  });
-});
+app.post("/api/login", (req,res)=>{
+    const {username, password} = req.body;
+    let token;
+    for(let user of users){
+        if(username == user.username && password==user.password){
+            token = jwt.sign({id:user.id,username:user.username}, secretKey, {expiresIn:'180s'})
+            break;
+        }
+    }
+    if (token) {
+        res.json({
+            success: true,
+            err: null,
+            token
+        });
+    }
+    else {
+        res.status(401).json({
+            success: false,
+            token: null,
+            err: 'Username or password is incorrect'
+        });
+    }
 
-app.get("/api/prices", jwtMW, (req, res) => {
-  res.json({
-    success: true,
-    myContent: "This is the price $7.99",
-  });
-});
+})
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.use(function (err, req, res, next) {
-  if (err.name === "UnauthorizedError") {
-    res.status(401).json({
-      success: false,
-      officialError: err,
-      err: "Username or password is incorrect 2",
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'Settings page works!!'
     });
-  } else {
-    next(err);
-  }
 });
 
-app.listen(port, () => {
-  console.log(`Serving at http://localhost:${port}`);
-});
+app.listen(PORT, ()=>{
+    console.log("Serving on PORT " + PORT);
+})
